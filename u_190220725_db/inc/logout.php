78db61d1ba2c6a987e8896367b0af3c9b8756637
popchop3 <58033519+popchop3@users.noreply.github.com>
2021-08-02 21:33:41 +0100

    Delete u_190220725_db directory
    Deleting old code to replace with new code on server

--- u_190220725_db/inc/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Starts the session
-session_start();
-//Immediately ends the session, so that session data is lost and the user is logged out. This will then enable another user to login
-session_destroy();
-echo 'You have been logged out. <a href="/u_190220725_db/index.php">Go back</a>';o
-?>
